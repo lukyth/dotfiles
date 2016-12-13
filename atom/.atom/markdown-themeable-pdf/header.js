190279cfedc93dd9c83d092c7f1958a613084a8a
Kanitkorn S <k.sujautra@gmail.com>
2016-12-13 15:34:30 +0700

    Add Atom config

---
+++ atom/.atom/markdown-themeable-pdf/header.js
@@ -0,0 +1,14 @@
+/**
+ * Your markdown-themeable-pdf custom header.
+ * After you have made any changes you need to reload or restart atom.
+ *
+ * The default file can be found in the folder ~/.atom/packages/markdown-themeable-pdf/templates
+ *
+ * @returns {{height: string, contents: string}}
+ */
+module.exports = function () {
+    return {
+        height: '2cm',
+        contents: '<div style="text-align: right;"><span>Created by</span> <img src="logo.png" alt="Logo" /> <span style="color: #EC4634; font-size: 120%; text-transform: uppercase;">markdown-themeable-pdf</span></div>'
+    };
+};
