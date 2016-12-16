d120e17f296c989c4e2aad2e345e9c70ede31d81
Kanitkorn S <k.sujautra@gmail.com>
2016-12-16 11:07:47 +0700

    Init brew setup script

---
+++ brew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+brew update
+brew upgrade
+
+brew install node
+brew install yarn
+
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install sublime-text
