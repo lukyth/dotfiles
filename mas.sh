b58de8e0a4e2f34247631475a07b3d9dc43aa39f
Kanitkorn S <k.sujautra@gmail.com>
2016-12-18 14:17:18 +0700

    Add missing App Store app
    - Giphy Capture
    - Keynote
    - Pages
    - Numbers

--- mas.sh
+++ mas.sh
@@ -5,3 +5,7 @@ mas install 1053031090 # Boxy
 mas install 539883307 # LINE
 mas install 441258766 # Magnet
 mas install 897118787 # Shazam
+mas install 668208984 # GIPHY CAPTURE
+mas install 409183694 # Keynote
+mas install 409201541 # Pages
+mas install 409203825 # Numbers
