d93fbadef1ea5ecaa6466593d690d347ac6cc0b5
Kanitkorn S <k.sujautra@gmail.com>
2017-06-02 16:22:21 +0700

    Update post installation script

--- post.sh
+++ post.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-git config --global user.name "Kanitkorn S."
-git config --global user.email "k.sujautra@gmail.com"
-git config --global color.ui true
-
 pip install --upgrade pip setuptools
 pip3 install --upgrade pip setuptools wheel
 git lfs install
 
-yarn global add pure-prompt
-
-ssh-keygen -t rsa -b 4096 -C "k.sujautra@gmail.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-pbcopy < ~/.ssh/id_rsa.pub
+npm install -g pure-prompt
