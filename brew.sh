df6566b45fed5fe7a045eb7cb4f18cbeddabddfe
Kanitkorn S <k.sujautra@gmail.com>
2018-09-07 12:28:47 +0700

    Update brew list

--- brew.sh
+++ brew.sh
@@ -33,22 +33,20 @@ brew tap caskroom/versions
 brew tap caskroom/fonts
 
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package quicklookase qlvideo # https://github.com/sindresorhus/quick-look-plugins
-brew cask install betterzip
 brew cask install google-chrome
 brew cask install firefox
 brew cask install iterm2-nightly
 brew cask install sublime-text
 brew cask install visual-studio-code-insiders
-brew cask install tower-beta
+brew cask install gitup
 brew cask install docker
-brew cask install minikube
-brew cask install virtualbox
 brew cask install paw
 brew cask install gpg-suite
 brew cask install mongodb-compass-beta
 brew cask install sequel-pro
 brew cask install imageoptim
 brew cask install zeplin
+brew cask install slack-beta 
 brew cask install handbrake
 brew cask install tunnelblick
 brew cask install font-fira-code font-open-sans font-roboto font-lato
@@ -59,14 +57,9 @@ brew cask install authy
 brew cask install hyperswitch
 brew cask install keka
 brew cask install java
-brew cask install android-file-transfer
 brew cask install iina
 brew cask install spotify
-brew cask install steam
-brew cask install battle-net
-brew cask install caprine
 brew cask install kap-beta
-brew cask install boostnote
-brew cask install webtorrent
+brew cask install bittorrent
 
 brew cleanup
