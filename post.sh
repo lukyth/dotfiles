4956b86912e9b65711eb2b85959b8bf6089c6a59
Kanitkorn S <k.sujautra@gmail.com>
2018-12-04 23:20:00 +0900

    Add git config symlink

--- post.sh
+++ post.sh
@@ -9,3 +9,9 @@ npm install -g pure-prompt
 mkdir ~/Desktop/Screenshots
 defaults write com.apple.screencapture location ~/Desktop/Screenshots
 killall SystemUIServer
+
+rm ~/.gitconfig
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+touch /Users/kanitkorn/.stCommitMsg
