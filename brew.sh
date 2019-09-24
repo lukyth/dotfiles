47d1db46f4997c7bcbbe7652b0c4e381f3a54c36
Kanitkorn S <k.sujautra@gmail.com>
2019-09-24 19:12:23 +0900

    Update brew list

--- brew.sh
+++ brew.sh
@@ -3,54 +3,33 @@
 brew update
 brew upgrade
 
-brew install yarn --without-node
+brew install yarn
 brew install mas
 brew install git
-brew install git-lfs
 brew install vim
-brew install htop
-brew install python
-brew install python3
-brew install go
-brew install tmux
-brew install tig
-brew install zsh
-brew install the_silver_searcher
-brew install wget
 brew install z
 brew install zsh-syntax-highlighting
-brew install mongodb
-brew install mysql
-brew install httpie
 brew install ripgrep
-brew install lastpass-cli --with-pinentry --with-doc
-brew install git-standup
-brew install fzf
-brew install tldr
-brew install watchman
 
-brew tap caskroom/versions
-brew tap caskroom/fonts
+brew tap homebrew/cask-fonts
+brew tap homebrew/cask-drivers
 
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package quicklookase qlvideo # https://github.com/sindresorhus/quick-look-plugins
 brew cask install google-chrome
 brew cask install firefox
-brew cask install iterm2-nightly
+brew cask install iterm2
 brew cask install sublime-text
-brew cask install visual-studio-code-insiders
+brew cask install visual-studio-code
 brew cask install gitup
 brew cask install docker
 brew cask install paw
 brew cask install gpg-suite
-brew cask install mongodb-compass-beta
-brew cask install sequel-pro
+brew cask install mongodb-compass
 brew cask install imageoptim
 brew cask install zeplin
 brew cask install paste
 brew cask install slack
-brew cask install homebrew/cask-drivers/logitech-options
+brew cask install logitech-options
 brew cask install handbrake
-brew cask install tunnelblick
 brew cask install font-fira-code font-open-sans font-roboto font-lato
 brew cask install alfred
 brew cask install istat-menus
@@ -61,8 +40,6 @@ brew cask install keka
 brew cask install java
 brew cask install iina
 brew cask install spotify
-brew cask install kap-beta
-brew cask install bittorrent
-brew cask install react-native-debugger
+brew cask install kap
 
 brew cleanup
