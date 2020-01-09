6499b32e5aef53821c2ed21e45706897d16c293a
Kanitkorn S <k.sujautra@gmail.com>
2020-01-09 17:31:44 +0900

    Update app store list

--- mas.sh
+++ mas.sh
@@ -4,7 +4,6 @@ mas install 485812721 # TweetDeck
 mas install 539883307 # LINE
 mas install 441258766 # Magnet
 mas install 897118787 # Shazam
-mas install 668208984 # GIPHY CAPTURE
 mas install 409183694 # Keynote
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
@@ -12,3 +11,4 @@ mas install 1153157709 # Speedtest
 mas install 1263070803 # Lungo
 mas install 1351639930 # Gifski
 mas install 1039633667 # Irvue
+mas install 967805235 # Paste
